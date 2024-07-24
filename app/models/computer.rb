cd15a74bb969c75239997a9c05648e0fc659c1eb
esbo <esbo@hotmail.fr>
2024-07-24 08:30:36 +0200

    ajout fonction model endoflife

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,2 +1,11 @@
 class Computer < ApplicationRecord
+  def end_of_life_class
+    if endoflife <= Date.today + 1.year
+      'end-life-danger' # Rouge
+    elsif endoflife <= Date.today + 2.years
+      'end-life-warning' # Orange
+    else
+      'end-life-success' # Vert
+    end
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,6 @@
 # config/importmap.rb
 pin "application", to: "application.js"
-#pin "@hotwired/turbo-rails", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo-rails@7.1.0/dist/turbo.esm.js"
-pin "@hotwired/turbo-rails", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@8.0.5/dist/turbo.es2017-umd.min.js"
+pin "@hotwired/turbo-rails", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@8.0.5/dist/turbo.umd.min.js"
 pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"
 pin "jquery-ui", to: "https://cdn.jsdelivr.net/npm/jquery-ui-dist/jquery-ui.min.js"
 pin "jquery-ui/widgets/datepicker", to: "https://cdn.jsdelivr.net/npm/jquery-ui-dist/jquery-ui.min.js"
