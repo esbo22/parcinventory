7ca281da5e9c3fd2454b94fbaa9958457a6177b4
esbo <esbo@hotmail.fr>
2024-08-11 23:03:15 +0200

    login page css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Défini la route root vers l'action home de PagesController
+  # Utilisation d'un contrôleur personnalisé pour les inscriptions Devise
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  # Définir la route root vers l'action home de PagesController
   root 'pages#home'
+
   # Autres routes
   resources :computers
   resources :clients
+
   # Route temporaire pour la page de connexion future pointant vers computers#index
   get 'login', to: 'computers#index'
 end
