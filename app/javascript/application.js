326e2cd70abfbcd07fc9713ab89eec677b710d99
johnny girault <johnny@MacBook-Air-de-johnny.local>
2024-09-24 22:27:09 +0200

    front back page edit user

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 import { Turbo } from "@hotwired/turbo-rails";
 import "bootstrap";
+import * as Popper from "@popperjs/core";
 import "./controllers/flash";
 import "./autocomplete";
 import "./tooltips"; 
